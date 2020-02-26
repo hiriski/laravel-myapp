4e3472492bf8c2acdbfe9c0e5b6110692aee623d
riskiwebid <riski.web.id@zoho.com>
2020-02-26 18:12:12 +0700

    add learn lang sentence migration and model

---
+++ database/migrations/2020_02_26_094135_create_learn_lang_sentence_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLearnLangSentenceCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('learn_lang_sentence_categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 50);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('learn_lang_sentence_categories');
+    }
+}
---
+++ database/migrations/2020_02_26_095045_create_learn_lang_sentences_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLearnLangSentencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('learn_lang_sentences', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('indonesia');
+            $table->text('english')->nullable();
+            $table->text('vietnam')->nullable();
+            $table->text('notes')->nullable();
+            $table->text('reference')->nullable();
+
+            /* Foreign key */
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedInteger('category_id');
+            $table->unsignedTinyInteger('level_id');
+            
+            $table->timestamp('deleted_at')->default(null);
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('learn_lang_sentence_categories')
+                ->onDelete('restrict')
+                ->onUpdate('restrict');
+
+            $table->foreign('level_id')
+                ->references('id')
+                ->on('learn_lang_sentence_levels');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('learn_lang_sentences');
+    }
+}
