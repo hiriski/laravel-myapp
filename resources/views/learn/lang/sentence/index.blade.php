cb29718bc8393ab0483e0067bcf8d729230de431
riskiwebid <juna.karty@my.gavilan.edu>
2020-02-29 20:51:47 +0700

    update index and add country flag

--- resources/views/learn/lang/sentence/index.blade.php
+++ resources/views/learn/lang/sentence/index.blade.php
@@ -1,25 +1,43 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-12 col-lg-8 mx-auto">
-            <table class="table table-sm">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>EN</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($sentence as $item)
-                    <tr>
-                        <td>{{ $item->indonesia }}</td>
-                        <td>{{ $item->english }}</td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="col-12 my-5">
+            <h3 class="text-center">Sentence</h3>
         </div>
+        @foreach ($sentence as $item)
+        <div class="col-12 col-md-6 col-lg-4 mb-3">
+            <div class="card sentence">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        <div class="flag">
+                            <span class="id"></span>
+                        </div>
+                        <p class="item">{{ $item->indonesia }}</p>
+                    </li>
+                    <li class="list-group-item">
+                        <div class="flag">
+                            <span class="en"></span>
+                        </div>
+                        <p class="item">{{ $item->english }}</p>
+                    </li>
+                    <li class="list-group-item">
+                    @if(!empty($item->vietnam))
+                        <div class="flag">
+                            <span class="vn"></span>
+                        </div>
+                        <p class="item">{{ $item->vietnam }}</p>
+                    @else
+                        <div class="flag">
+                            <span class="vn"></span>
+                        </div>
+                        <p class="item">nothing..</p>
+                    @endif
+                    </li>
+                </ul>
+            </div>
+        </div>
+        @endforeach
     </div>
 </div>
 @endsection
