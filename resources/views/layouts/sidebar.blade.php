c58cc234e575ef3d7daf5e64ee7a1bad8381d7b4
riskiwebid <juna.karty@my.gavilan.edu>
2020-03-10 15:26:31 +0700

    show pic on sidebar

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,6 +1,25 @@
 <div id="sidebar" class="left">
     <div class="sidebar-inner">
         <div class="sidebar_menu">
+            <div class="sidebar-header">
+                <div class="sidebar-header-inner">
+                    <div class="profile-pic">
+                    @guest
+                        <img src="Nothing" alt="">
+                    @else
+                        <img src="{{ url('') }}/{{ Auth::user()->profile->image_url }}"
+                                alt="{{ Auth::user()->profile->first_name }}"
+                                class="img-fluid">
+                    @endguest                
+                    </div>
+                    <div class="profile-name">
+                        <span>@lang('app.hello') 👋</span>
+                        <h3>
+                            {{ Auth::user()->profile->first_name }}  {{ Auth::user()->profile->last_name }}
+                        </h3>
+                    </div>
+                </div>
+            </div>
             <div class="sidebar-menu">
             <h6 class="sidebar-header">Learning :)</h6>
             <a class="sidebar-menu-item" href="{{ route('sentence.index') }}">Sentence</a>
