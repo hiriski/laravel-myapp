0b871642731fbee455e3c50216b12ce0dc5ca1b6
riskiwebid <juna.karty@my.gavilan.edu>
2020-03-11 12:24:22 +0700

    update greeting method

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,16 +6,15 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
 use Carbon\Carbon;
+use App;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
         //
     }
 
@@ -29,16 +28,41 @@ class AppServiceProvider extends ServiceProvider
 
         /** greeting */
         Blade::directive('greeting', function () {
-            $hours = (int)Carbon::parse(Carbon::now())->format('H');
-            $message = NULL;
+            $hours      = (int)Carbon::parse(Carbon::now())->format('H');
+            $locale     = App::getLocale();
+            $message    = NULL;
             if($hours >= 3 && $hours < 10) {
-                $message = "Good Morning";
+                if($locale == 'vn') {
+                    $message = 'Chào Buổi sáng';
+                }
+                else if($locale == 'id') {
+                    $message = 'Selamat Pagi';
+                }
+                else {
+                    $message = 'Good Morning';
+                }
             }
             else if($hours > 10 && $hours <= 17) {
-                $message = "Good Afternoon";
+                if($locale == 'vn') {
+                    $message = 'Chào Buổi chiều';
+                }
+                else if($locale == 'id') {
+                    $message = 'Selamat Siang';
+                }
+                else {
+                    $message = 'Good Afternoon';
+                }
             }
             else {
-                $message = "Good Evening";
+                if($locale == 'vn') {
+                    $message = 'Chào Buổi tối';
+                }
+                else if($locale == 'id') {
+                    $message = 'Selamat Malam';
+                }
+                else {
+                    $message = 'Good Evening';
+                }
             }
             return $message;
         });
