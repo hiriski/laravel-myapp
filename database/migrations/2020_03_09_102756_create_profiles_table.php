b9e6c4148447be3345d1992fae766256f1b22802
riskiwebid <juna.karty@my.gavilan.edu>
2020-03-11 17:39:00 +0700

    add role and social media to the profile table

--- database/migrations/2020_03_09_102756_create_profiles_table.php
+++ database/migrations/2020_03_09_102756_create_profiles_table.php
@@ -14,6 +14,7 @@ class CreateProfilesTable extends Migration {
         Schema::create('profiles', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('user_id');
+            $table->string('role')->default('user');
             $table->string('slug')->unique();
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
@@ -21,6 +22,15 @@ class CreateProfilesTable extends Migration {
             $table->string('image_mid_url')->nullable();
             $table->string('image_sm_url')->nullable();
             $table->string('language')->default('en');
+
+            $table->text('facebook_url')->nullable();
+            $table->text('instagram_url')->nullable();
+            $table->text('twitter_url')->nullable();
+            $table->text('linkedin_url')->nullable();
+            $table->text('github_url')->nullable();
+            $table->text('codepen_url')->nullable();
+            $table->text('whatsapp')->nullable();
+
             $table->timestamps();
 
             $table->foreign('user_id')
--- database/seeds/ProfilesTableSeeder.php
+++ database/seeds/ProfilesTableSeeder.php
@@ -16,21 +16,33 @@ class ProfilesTableSeeder extends Seeder {
         $users_profile = array(
             array(
                 'user_id'       => 1,
+                'role'          => 'admin',
                 'slug'          => Str::slug('anh'),
                 'first_name'    => 'Riski',
                 'last_name'     => 'Web ID',
                 'image_url'     => 'images/profiles/anh.png',
                 'image_sm_url'  => 'images/profiles/anh-sm.png',
-                'language'      => 'en'
+                'language'      => 'en',
+                'facebook_url'  => 'https://facebook.com/hi.riski',
+                'instagram_url' => 'https://www.instagram.com/riskiwebid/',
+                'github_url'    => 'https://github.com/riskiwebid',
+                'codepen_url'   => 'https://codepen.io/riskiwebid',
+                'whatsapp'      => '6282122281813',
             ),
             array(
                 'user_id'       => 2,
+                'role'          => 'user',
                 'slug'          => Str::slug('em'),
                 'first_name'    => 'Em',
                 'last_name'     => 'Xinh Đẹp',
                 'image_url'     => 'images/profiles/em.jpg',
                 'image_sm_url'  => 'images/profiles/em-sm.jpg',
-                'language'      => 'vn'
+                'language'      => 'vn',
+                'facebook_url'  => 'https://web.facebook.com/profile.php?id=100005481379207',
+                'instagram_url' => 'https://www.instagram.com/ngoccute_23/',
+                'github_url'    => NULL,
+                'codepen_url'   => NULL,
+                'whatsapp'      => NULL,
             )
         );
         DB::table($this->tableName)->insert($users_profile);
