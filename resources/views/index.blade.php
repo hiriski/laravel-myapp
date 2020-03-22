acf2066fc55ea4efee9080a7bef3128bbd93ffc1
riskiwebid <riski.web.id@zoho.com>
2020-03-22 20:12:57 +0700

    fix sidebar for guest

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,18 @@
-<h1>Index page!</h1>
+@extends('layouts.app')
+
+@section('content')
+	<div class="container">
+		<div class="row justify-content-center mt-5 mb-3">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-body">
+						<h1 class="card-title">
+							Hello World!
+						</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<example-component></example-component>
+@endsection
