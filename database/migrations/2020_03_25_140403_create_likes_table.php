10435a009bdee281d35de16b4749167106bdc488
riskiwebid <riski.web.id@zoho.com>
2020-03-25 16:03:43 +0700

    add likes table migration

---
+++ database/migrations/2020_03_25_140403_create_likes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLikesTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('likeable_id');
+            $table->string('likeable_type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('likes');
+    }
+}
