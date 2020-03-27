4538571ebf5e52fbeb55d57f8769f304dab5a7e9
riskiwebid <riski.web.id@zoho.com>
2020-03-28 01:34:19 +0700

    protect learn lang under middleware auth

--- app/Http/Controllers/Learn/Lang/PhraseController.php
+++ app/Http/Controllers/Learn/Lang/PhraseController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 use App\Models\Learn\Lang\Phrase;
 
 class PhraseController extends Controller {
+
+	public function __construct() {
+		return $this->middleware('auth');
+	}
+
 	public function index() {
 		return view('learn.lang.phrase');
     }   
--- app/Http/Controllers/Learn/Lang/SentenceController.php
+++ app/Http/Controllers/Learn/Lang/SentenceController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 use App\Models\Learn\Lang\Sentence;
 
 class SentenceController extends Controller {
+
+    public function __construct() {
+		return $this->middleware('auth');
+    }
+    
     public function index() {
         return view('learn.lang.sentence');
     }
