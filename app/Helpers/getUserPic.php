44ab67e97cbb12a79e5775a204a79d70300c04de
riskiwebid <riski.web.id@zoho.com>
2020-04-03 21:32:18 +0700

    fix undefined image if user don't have profile

--- app/Helpers/getUserPic.php
+++ app/Helpers/getUserPic.php
@@ -13,11 +13,15 @@ if(!function_exists('getUserPic')) {
 	function getUserPic($userId, $size = NULL) {
 		$path 			= url('images/profiles/');
 		$user 			= User::where('id', $userId)->first();
-		$userPic;
-		switch($size) {
-			case "sm"	: $userPic  = $path . '/' . $user->profile->image_sm; break;
-			case "md" 	: $userPic  = $path . '/' . $user->profile->image_md; break;
-			default 	: $userPic  = $path . '/' . $user->profile->image;    break;
+		$userPic 		= '/images/sm.jpg';
+
+		/** Uji dulu apakah user ini ada profilenya atau nggak */
+		if($user->profile !== NULL) {
+			switch($size) {
+				case "sm"	: $userPic  = $path . '/' . $user->profile->image_sm; break;
+				case "md" 	: $userPic  = $path . '/' . $user->profile->image_md; break;
+				default 	: $userPic  = $path . '/' . $user->profile->image;    break;
+			}
 		}
 		return $userPic;
 	}
