2ecd9aeba69571370470509042135f1dfc38fdd0
riskiwebid <riski.web.id@zoho.com>
2020-04-07 15:50:23 +0700

    update syntax color prism

--- public/js/app.js
+++ public/js/app.js
@@ -98,8 +98,8 @@ var changeTitle = function changeTitle() {
   title.textContent = String.fromCodePoint(0x211B, 0x1D4BE, 0x1D4C8, 0x1D4C0, 0x1D4BE);
 };
 
-window.addEventListener('DOMContentLoaded', function () {
-  removePreload(); // changeTitle();
+window.addEventListener('DOMContentLoaded', function () {// removePreload();
+  // changeTitle();
 });
 
 var removePreload = function removePreload() {
--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ const changeTitle = () => {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-    removePreload();
+    // removePreload();
     // changeTitle();
 })
 
