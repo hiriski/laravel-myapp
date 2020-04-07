ec6c8d5d81658270f7cb3721261e514c88cf4202
riskiwebid <riski.web.id@zoho.com>
2020-04-07 17:34:05 +0700

    optimize seo for category blog page

--- app/Http/Controllers/BlogCategoryController.php
+++ app/Http/Controllers/BlogCategoryController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\BlogCategory as Category;
 
-class BlogCategoryController extends Controller {
+/** SEO */
+use Artesaos\SEOTools\Facades\SEOMeta;
+use Artesaos\SEOTools\Facades\OpenGraph;
 
+class BlogCategoryController extends Controller {
 
     /**
      * Display a listing of the resource.
@@ -15,8 +18,25 @@ class BlogCategoryController extends Controller {
      */
     public function index() {
         $categories = Category::all();
-        $title      = 'Blog Category';
-        return view('front.blog.categories.index', compact('categories', 'title'));
+        /** SEO META     */
+        SEOMeta::setTitle('Blog Categories')
+                ->setDescription('Deskripsi halaman blog index disini')
+                ->setKeywords('Keyword1, keyword2 and more')
+                ->addMeta('author', 'Riski' . ', hi@riski.web.id', 'name')
+                ->addMeta('copyright', 'Riski Web ID', 'name')
+                ->addMeta('designer', 'Riski', 'name')
+                ->setCanonical(route('blog.category.index'));
+
+        /** OpenGraph */
+        OpenGraph::setTitle('Blog Category')
+                ->setDescription('Deskripsi halaman blog index disini')
+                ->setUrl(route('blog.category.index'))
+                ->setSiteName('Riski Web ID')
+                ->addImage(getBlogImage('image-jpg'))
+                ->addProperty('locale', 'id-ID')
+                ->addProperty('locale:alternate', ['id-ID', 'en-US']);
+
+        return view('front.blog.categories.index', compact('categories'));
     }
 
     /**
@@ -27,8 +47,27 @@ class BlogCategoryController extends Controller {
      */
     public function show($slug) {
         $category = Category::with(['blogs' => function($query) {
-            $query->orderBy('created_at', 'DESC');
+            $query->orderBy('created_at', 'DESC')->paginate(6);
         }])->where('slug', $slug)->firstOrFail();
+
+        /** SEO META     */
+        SEOMeta::setTitle($category->name)
+                ->setDescription($category->description)
+                ->setKeywords($category->keywords)
+                ->addMeta('author', 'Riski' . ', hi@riski.web.id', 'name')
+                ->addMeta('copyright', 'Riski Web ID', 'name')
+                ->addMeta('designer', 'Riski', 'name')
+                ->setCanonical(route('blog.show', $category->slug));
+
+        /** OpenGraph */
+        OpenGraph::setTitle($category->name)
+                ->setDescription($category->description)
+                ->setUrl(route('blog.show', $category->slug))
+                ->setSiteName('Riski Web ID')
+                // ->addImage(getBlogImage($category->featured_image))
+                ->addProperty('type', 'article')
+                ->addProperty('locale', 'id-ID')
+                ->addProperty('locale:alternate', ['id-ID', 'en-US']);
 
         return view('front.blog.categories.show', compact('category'));
     }
