8f439e05ddea452980f0e16b80022852d4b35929
riskiwebid <riski.web.id@zoho.com>
2020-04-07 11:51:08 +0700

    add blog option from database setting

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -5,35 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\FeaturePost;
 
+use App\Models\Setting;
+use App\Models\Blog;
+
+
 /** SEO */
 use Artesaos\SEOTools\Facades\SEOMeta;
 use Artesaos\SEOTools\Facades\OpenGraph;
 
 class IndexController extends Controller {
-	
-	public function __construct() {
-	}
 
-	public function index() {
+    public function index() {
 		$feature_posts = FeaturePost::all();
+		$setting = Setting::findOrFail(1);
+		$posts = Blog::with(['category'])->where('status_id', 1)
+		->orderBy('created_at', 'DESC')->paginate(6);
+                
 		/** SEO META     */
         SEOMeta::setTitle('Home')
-                ->setDescription('Deskripsi homepage disini')
-                ->setKeywords('Keyword1, keyword2 and more')
-                ->addMeta('author', 'Riski' . ', hi@riski.web.id', 'name')
-                ->addMeta('copyright', 'Riski Web ID', 'name')
-                ->addMeta('designer', 'Riski', 'name')
-                ->setCanonical(route('index'));
+			->setDescription('Deskripsi homepage disini')
+			->setKeywords('Keyword1, keyword2 and more')
+			->addMeta('author', 'Riski' . ', hi@riski.web.id', 'name')
+			->addMeta('copyright', 'Riski Web ID', 'name')
+			->addMeta('designer', 'Riski', 'name')
+			->setCanonical(route('index'));
 
         /** OpenGraph */
         OpenGraph::setTitle('Homepage')
-                ->setDescription('Deskripsi halaman home disini')
-                ->setUrl(route('index'))
-                ->setSiteName('Riski Web ID')
-                ->addImage(getBlogImage('image-jpg'))
-                ->addProperty('locale', 'id-ID')
-				->addProperty('locale:alternate', ['id-ID', 'en-US']);
-				
-		return view('front.index', compact('feature_posts'));
-	}
+			->setDescription('Deskripsi halaman home disini')
+			->setUrl(route('index'))
+			->setSiteName('Riski Web ID')
+			->addImage(getBlogImage('image-jpg'))
+			->addProperty('locale', 'id-ID')
+			->addProperty('locale:alternate', ['id-ID', 'en-US']);
+			
+		return view('front.index', compact('feature_posts', 'setting', 'posts'));
+    }
 }
