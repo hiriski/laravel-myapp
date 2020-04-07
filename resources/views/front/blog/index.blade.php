4660b9957a8b4151adde2c33360dbd5639795bed
riskiwebid <riski.web.id@zoho.com>
2020-04-07 17:35:24 +0700

    update front global

--- resources/views/front/blog/index.blade.php
+++ resources/views/front/blog/index.blade.php
@@ -1,7 +1,4 @@
 @extends('front.layouts.app')
-@section('title', 'Blog')
-@section('description', 'Blog index riski web id')
-@section('keywords', 'Blog riski web id')
 @section('content')
 <div class="Anh">
     <div class="__o__">
