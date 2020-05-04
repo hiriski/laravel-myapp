5533f061a51f090f9fcfb4c68068d3c692b2ffca
riskiwebid <juna.karty@my.gavilan.edu>
2020-05-04 21:23:15 +0700

    commit abstract

---
+++ resources/views/front/layouts/footer-v2.blade.php
@@ -0,0 +1,10 @@
+<footer id="main" class="v2">
+  <div class="_fm">
+    <div class="__">
+      <div class="c__">
+        <p><span>© 2020</span> <a href="">Riski Web ID</a></p>
+        <p>Crafted with <i class="material-icons">favorite</i></p>
+      </div>
+    </div>
+  </div>
+</footer>
